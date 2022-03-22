90e2ca33bb5c1322b0b39ec40d2318d76b4ea708
lingj1206 <lingj1206@masseyhigh.school.nz>
2022-03-23 10:45:00 +1300

    Update 03_perim_area_calculator.py

---
+++ 03_perim_area_calculator.py
@@ -0,0 +1,25 @@
+def num_check(question):
+    valid = False
+    while not valid:
+
+        error = ("Please enter a number that is more than zero")
+
+        try:
+
+            response = float(input(question))
+            
+            if response > 0:
+                return response
+
+            else:
+                print(error)
+                print()
+        
+        except ValueError:
+            print(error)
+
+
+
+width = num_check("width: ")
+height = num_check("height: ")
+
