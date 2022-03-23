38a4972ea4bbe1db12d6e61d319767214e148967
lingj1206 <lingj1206@masseyhigh.school.nz>
2022-03-23 21:26:23 +1300

    Create 04_area_perim_calculator_v2.py

---
+++ 04_area_perim_calculator_v2.py
@@ -0,0 +1,40 @@
+
+
+def num_check(question):
+    valid = False
+    while not valid:
+
+        error = ("Please enter a number that is more than zero")
+
+        try:
+
+            response = float(input(question))
+            
+            if response > 0:
+                return response
+
+            else:
+                print(error)
+                print()
+        
+        except ValueError:
+            print(error)
+
+
+keep_going = ""
+while keep_going == "":
+
+    width = num_check("width: ")
+    height = num_check("height: ")
+
+    area = width * height
+    perimeter = 2 * (width + height)
+
+    print("perimeter: {} units".format(perimeter))
+    print("area: {} squared units".format(area))
+    print()
+
+    keep_going = input("Press <enter> to keep going or any key to quit")
+
+print()
+print("Thanks for using the area and perimeter calculator")
